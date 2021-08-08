df29f2b186ca68997fb30f32944c5359a6513f33
Nitu Balwani <balwaninitu@gmail.com>
2021-08-08 20:39:29 +0800

    First JS Commit

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,15 @@
+// 
+
+let age = 23;
+let year = 2019;
+
+console.log(age, year);
+
+//ovewrite the variable
+age = 30;
+console.log(age);
+
+//const can not overwrite
+const points = 70;
+//points = 80;
+console.log(points);
