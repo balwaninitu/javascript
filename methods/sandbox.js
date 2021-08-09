f961bd15b275f3730614444a428c7ca8e1e984f5
Nitu Balwani <balwaninitu@gmail.com>
2021-08-09 11:19:24 +0800

    methods

---
+++ methods/sandbox.js
@@ -0,0 +1,13 @@
+//function
+
+const greet = () => 'hello!';
+
+const result = greet();
+console.log(result);
+
+//methods
+const name = 'nitu';
+
+let result1 = name.toUpperCase();
+console.log(result1);
+
