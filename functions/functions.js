c0bb0ead0882b2d21b05b4fb6bc50b4a9efb99fd
Nitu Balwani <balwaninitu@gmail.com>
2021-08-09 10:00:37 +0800

    parameters in function

--- functions/functions.js
+++ functions/functions.js
@@ -28,3 +28,11 @@ const speak = function (name = 'Nitu', time = 'Night') {
 
 speak('titu');
 
+const calcArea = function (radius) {
+    let area = 3.14 * radius ** 2;
+    return area;
+};
+
+const area = calcArea(5);
+console.log(area);
+
