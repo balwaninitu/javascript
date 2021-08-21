ef22bf1f5b8a5ed785b0b561c49bce4b40b4686a
Nitu Balwani <balwaninitu@gmail.com>
2021-08-21 14:41:04 +0800

    animation

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -45,6 +45,16 @@ form.addEventListener('submit', e => {
   result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-done');
 
+  let output = 0;
+  const timer = setInterval(() => {
+      result.querySelector('span').textContent = `${output}%`;
+      if(output === score){
+          clearInterval(timer);
+      }else {
+          output++;
+      }
+  }, 10);
+
 });
 
 //window object
@@ -52,3 +62,13 @@ form.addEventListener('submit', e => {
 // setTimeout(() => {
 //     alert('hello, ninjas');
 // }, 3000);
+
+// let i = 0;
+// const timer = setInterval(() => {
+//     console.log('hello');
+//     i++;
+//     if(i === 5){
+// clearInterval(timer);
+//     }
+    
+// }, 1000);
