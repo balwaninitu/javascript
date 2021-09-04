8ea8bee9519886e6e31ce0aa5dbccfa6fc0d4348
Nitu Balwani <balwaninitu@gmail.com>
2021-09-04 17:21:37 +0800

    Array- Map method

---
+++ Array/02-map-method/sandbox.js
@@ -0,0 +1,29 @@
+//map method
+
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+
+const salePrices = prices.map(price => price / 2);
+console.log(salePrices);
+
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50}
+  ];
+//get prices half if over 30
+  const saleProducts = products.map(product => {
+      if(product.price > 30){
+          //below method will disrupt original array
+        //   product.price = product.price / 2;
+        //   return product;
+        //below method will not disrupt original array
+          return {name:product.name, price: product.price / 2};
+      } else {
+return product;
+      }
+  });
+
+  console.log(saleProducts, products);
+  
