e3568c608ca7a4aaff4ff1e043bc4573e1eb6941
Nitu Balwani <balwaninitu@gmail.com>
2021-09-27 08:28:30 +0800

    Asynchronous JS

--- http-requests/sandbox.js
+++ http-requests/sandbox.js
@@ -1,6 +1,8 @@
  //make http request to get data from another server
  //we make these requests to API end points
 
+//wrap up below code in function for reusability
+const getTodos = (callback) => {
  //create request object
  const request = new XMLHttpRequest();
 
@@ -10,17 +12,38 @@
   request.addEventListener('readystatechange', () => {
       //console.log(request, request.readyState);
       if(request.readyState == 4 && request.status == 200){
+          callback(undefined, request.responseText);
           //it will return data on console
-          console.log(request.responseText)
+          //console.log(request.responseText)
       } else if(request.readyState == 4) {
           console.log('could not fetch the data');
+           callback('could not fetch data', undefined);
 
       }
-  })
-
-
+  });
 
  //setting up request
  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
  //send request
  request.send();
+};
+
+console.log(1);
+console.log(2);
+
+//define callback function to do specific task with data
+//specify callback function
+getTodos((err, data) => {
+    console.log('callback fired');
+    //console.log(err, data)
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+
+});
+
+console.log(3);
+console.log(4);
+
