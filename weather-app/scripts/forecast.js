8ff63fbcc456201612876cd6a0c79e1a4dbd0f14
Nitu Balwani <balwaninitu@gmail.com>
2021-10-02 16:18:54 +0800

    get weather api call

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,6 +1,21 @@
 
 const key = '2nCYFTjSdxPEP5dNtOVApLPEq8cAU3zk';
 
+//get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return (data)[0];
+
+}
+
+
+//get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -14,6 +29,18 @@ const getCity = async (city) => {
 
 };
 
-getCity('mumbai')
-.then(data => console.log(data))
+// getCity('mumbai')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+getCity('mumbai').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+})
 .catch(err => console.log(err));
+
+
+
+
+//getWeather("204842");
