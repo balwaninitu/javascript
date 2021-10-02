a2a143bec3b3d45bfe9a5dd8bb31b570099f2436
Nitu Balwani <balwaninitu@gmail.com>
2021-10-02 14:58:29 +0800

    throwing-and -catching error

--- async-and-wait/sandbox.js
+++ async-and-wait/sandbox.js
@@ -3,17 +3,18 @@
 const getTodos = async () => {
 
     const response = await fetch('json/luigi.json');
+
+    if(response.status != 200){
+        throw new Error('cannot fetch the data');
+    }
     const data = await response.json();
 
     return data;
 
 };
 
-console.log(1);
-console.log(2);
-
 getTodos()
-.then(data => console.log('resolved:', data));
+.then(data => console.log('resolved:', data))
+.catch(err => console.log('rejected:', err.message));
     
-    console.log(3);
-    console.log(4);
+   
