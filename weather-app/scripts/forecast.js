4d8992ed33d55adf0e134c1f97f3043a1ddbf779
Nitu Balwani <balwaninitu@gmail.com>
2021-10-02 16:04:32 +0800

    Get city API call

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,2 +1,19 @@
 
 const key = '2nCYFTjSdxPEP5dNtOVApLPEq8cAU3zk';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    //return promise
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return (data[0]);
+
+};
+
+getCity('mumbai')
+.then(data => console.log(data))
+.catch(err => console.log(err));
