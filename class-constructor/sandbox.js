50fdcd75a95812153800aeeb6ef8b636fdf54489
Nitu Balwani <balwaninitu@gmail.com>
2021-10-09 18:27:54 +0800

    constructor(under the hood)

--- class-constructor/sandbox.js
+++ class-constructor/sandbox.js
@@ -1,51 +1,23 @@
-class User{
-    constructor(username, email){
-        //set up properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    //set up method
-    login(){
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout(){
-        console.log(`${this.username} just logged out`); 
-        return this; 
-    }
-    incScore(){
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
-
-    }
-
+//constructor functions
+function User(username, email){
+this.username = username;
+this.email = email;
+this.login = function(){
+    console.log(`${this.username} has logged in`);
+}
 }
 
-class Admin extends User{
-    constructor(username, email, title){
-        super(username, email);
-        this.title = title;
 
-    }
-
-deleteUser(user){
-    users = users.filter(u =>  u.username != user.username);
-}
-
-}
-
+// class User{
+//     constructor(username, email){
+//         //set up properties
+//         this.username = username;
+//         this.email = email;
+//         this.score = 0;
+//     }
+   
 const userOne = new User('mario', 'mario@co.sg');
 const userTwo = new User('luigi', 'luigi@co.sg');
-const userThree = new Admin('nitu', 'nitu@co.sg', 'black-belt-ninja');
 
-console.log(userThree);
-
-console.log(userOne, userTwo, userThree);
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
+console.log(userOne, userTwo);
+userOne.login();
