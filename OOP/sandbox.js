0c134d20adcf67dcb24ed055ec089f59b1213554
Nitu Balwani <balwaninitu@gmail.com>
2021-10-09 17:21:22 +0800

    Object literals

---
+++ OOP/sandbox.js
@@ -0,0 +1,32 @@
+//object literal notation to create object
+
+const userOne = {
+    username: 'nitu',
+    email: 'nitu@sg.co.sg',
+    login(){
+        console.log('the user logged in');   
+    },
+    logout(){
+        console.log('the user logged out');
+    }
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
+
+const userTwo = {
+    username: 'ritu',
+    email: 'ritu@sg.co.sg',
+    login(){
+        console.log('the user logged in');   
+    },
+    logout(){
+        console.log('the user logged out');
+    }
+};
+
+console.log(userTwo.email, userTwo.username);
+userTwo.login();
+
+//better approach
+const userThree = new User('ridu@sg.co.sg', 'ridu');
