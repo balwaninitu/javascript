dad7b24522868408bbe07f40f3c20be7c420596b
Nitu Balwani <balwaninitu@gmail.com>
2021-10-28 15:30:35 +0800

    set up firebase and bootstrap

---
+++ connecting-to-firestore/sandbox.js
@@ -0,0 +1,9 @@
+// db.collection('recipes').get().then((snapshot) => {
+//     //when we have the data
+//     snapshot.docs.array.forEach(doc => {
+//         console.log(doc.data());
+        
+//     });
+// } ).catch(err => {
+//     console.log(err);
+// });
