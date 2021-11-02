13213d22d3116ff6492098fb667c201c48aaced3
Nitu Balwani <balwaninitu@gmail.com>
2021-11-02 16:38:51 +0800

    firebase

--- connecting-to-firestore/sandbox.js
+++
@@ -1,9 +0,0 @@
-// db.collection('recipes').get().then((snapshot) => {
-//     //when we have the data
-//     snapshot.docs.array.forEach(doc => {
-//         console.log(doc.data());
-        
-//     });
-// } ).catch(err => {
-//     console.log(err);
-// });
