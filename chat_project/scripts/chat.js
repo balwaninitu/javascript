98c983091fa90e4f4d2ac20398d4c71c3b5800f9
Nitu Balwani <balwaninitu@gmail.com>
2021-11-02 16:38:32 +0800

    chat project

---
+++ chat_project/scripts/chat.js
@@ -0,0 +1,16 @@
+//adding new chat documents
+//setting up a real-time listener to get new chats
+//updating the username
+//updating the room
+
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+
+    }
+}
+
+const chatroom = new Chatroom('gaming', 'shaun');
+console.log(chatroom);
